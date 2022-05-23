ccbc42fd0f76c3e59378de6fbdccf171c85e8452
Franco Van Zyl <franco@source.africa>
2022-05-23 21:55:22 +0200

    End of section 22

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,6 +34,7 @@ namespace API.Controllers
             _ProductsRepo = ProductsRepo;
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery]ProductSpecParams productParams)
@@ -53,6 +54,7 @@ namespace API.Controllers
             productParams.PageSize, totalItems, data));
         }
 
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -67,12 +69,14 @@ namespace API.Controllers
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _ProductBrandRepo.ListAllAsync());
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
